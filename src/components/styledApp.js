f96e5d9de93f767895953cb62193c5919a8fc19b
Inna Makarenko <macksi@ukr.net>
2023-05-02 01:52:24 +0300

    upd styles

--- src/components/styledApp.js
+++ src/components/styledApp.js
@@ -2,11 +2,12 @@ import 'normalize.css';
 
 import styled from 'styled-components';
 export const StyledContainerApp = styled.div`
-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        max-width: 600px;
-        align-items: center;
-        font-size: 28px;
-        color: #010101;  
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  align-items: center;
+  font-size: 28px;
+  color: #010101;
+  margin: 0 auto;
 `;
